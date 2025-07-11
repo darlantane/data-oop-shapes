e08d057fb4c2d5d22d887100bcd32949a72bbb8c
darlan tane <darlan.tane@gmail.com>
2025-07-11 12:18:37 +0200

    OOP

--- shape.py
+++ shape.py
@@ -1,2 +1,46 @@
 # pylint: disable=too-few-public-methods missing-module-docstring
-pass  # YOUR CODE HERE
+
+# Base class: Shape
+
+from math import pi
+
+class Shape:
+    def __init__(self, color, name):
+        self.color = color
+        self.name = name
+
+    def say_name(self):
+        return f"My name is {self.name}."
+
+
+# Rectangle class
+class Rectangle(Shape):
+    def __init__(self, color, name, width, height):
+        super().__init__(color, name)
+        self.width = width
+        self.height = height
+
+    def say_name(self):
+        return f"My name is {self.name} and I am a rectangle."
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+
+# Circle class
+class Circle(Shape):
+    def __init__(self, color, name, radius):
+        super().__init__(color, name)
+        self.radius = radius
+
+    def say_name(self):
+        return f"My name is {self.name} and I am a circle."
+
+    def area(self):
+        return pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * pi * self.radius
